0b382d53c6146f1cadc349a8d7b399be799c4733
dicka_ismaji <dicka_ismaji@quick.com>
2019-08-07 15:19:44 +0700

    ok

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,28 @@ $(document).ready(function(){
    $('.main').show("slide", {
       direction: "up"
    }, 2000);
+   $('#btn-top').fadeOut('fast');
+
 });
 
 function toggle(){
   console.log("okey");
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    $('#btn-top').fadeOut('slow');
+  } else {
+    $('#btn-top').fadeIn('slow');
+    setTimeout(function () {
+      $('#btn-top').fadeOut();
+    }, 4000);
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function toggleSide(){
+  $('#toggle').toggleClass('fa fa-arrow-left');
 }
